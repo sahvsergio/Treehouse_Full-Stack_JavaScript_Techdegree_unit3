3516a435d9a160fbabfa083988b6898a44e9dd0b
sahvsergio <sergioandresherrera@gmail.com>
2025-01-26 17:32:54 -0500

    add focus on load for name field

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//select all the current elements in the page 
+let formElement=document.querySelector('form');
+let nameInput=document.getElementById('name');
+let otherJob=document.getElementById('other-job-role');
+let jobTitle=document.getElementById('title');
+let colorSelect=document.getElementById('color');
+
+function focusName(){
+    /*When the page first loads, the first text field should have 
+the focus state by default to prompt the use*/
+    //name should start wit
+    nameInput.focus();
+}
+
+
+focusName();hadndleJobtitle();
