bb17772bd8fae89fc6ebda3186761bf68c4063f2
sahvsergio <sergioandresherrera@gmail.com>
2025-01-27 21:11:28 -0500

    complete handleColor function

--- js/script.js
+++ js/script.js
@@ -5,15 +5,18 @@ let otherJob=document.getElementById('other-job-role');
 let jobTitle=document.getElementById('title');
 let design=document.getElementById('design');
 let colorSelect=document.getElementById('color');
+let colorOptions=document.querySelectorAll('option[data-theme]')
+let totalCost=document.getElementById('activities-cost')
 
 function focusName(){
-    /*When the page first loads, the first text field should have 
+    /*When the page first loads, the first text field should have
 the focus state by default to prompt the use*/
     //name should start wit
     nameInput.focus();
 }
+
 function jobHandler(){
-    //hide the other job input text by default  
+    //hide the other job input text by default
    otherJob.hidden=true;
    jobTitle.addEventListener('change',(e)=>{
     //collect the target value
@@ -22,58 +25,38 @@ function jobHandler(){
     if (titleValue==='other'){
         otherJob.hidden=false;
 
-
     }else{
         otherJob.hidden=true;
-      
-
-
-
     }
-  
-    
-    
-   });
+});
 }
 
-   
 
 function handleColor(){
     colorSelect.disabled=true;
-    
-     design.addEventListener('change',(e)=>{
-            let designValue=e.target.value;
-            console.log(designValue);
-            //js puns or heart js
-            if (designValue){
-                colorSelect.disabled=false;
-
-            }else{
-                colorSelect.disabled=true;
-            }
-            
+    design.addEventListener('change',(e)=>{
         
-     });
+        for(let i=0;i<colorOptions.length;i++){
+            if(design.value!==colorOptions[i].getAttribute("data-theme")){
+                colorOptions[i].hidden=true;
+                colorOptions[i].disabled=true;
+            }
+            else{
+                colorSelect.disabled=false;
+                colorOptions[i].hidden=false;
+                colorOptions[i].disabled=false;
+
+            }
+        }
+        if(colorSelect.value!=''){
+
+            colorSelect.value='Select Theme';
+        }
+    });
+}
 
 
-}  
 
-
-
-
-   
-     
-
-    
-
-
-
-
-   
-    
-
-
- 
 
 focusName();
 jobHandler();
